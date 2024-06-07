a6cbbfdf0ef51906377547291ec3cff0c41d4bf5
kennypon <nigiaga@gmail.com>
2024-06-07 06:39:41 -0700

    add practice kpiconfig

--- src/main/java/com/investing/start/StartApplication.java
+++ src/main/java/com/investing/start/StartApplication.java
@@ -1,13 +1,24 @@
 package com.investing.start;
 
+import com.investing.start.config.kpiConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StartApplication {
+public class StartApplication implements CommandLineRunner {
+	private kpiConfig kpis;
 
+	public  StartApplication(kpiConfig kpis) {
+		this.kpis = kpis;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(StartApplication.class, args);
 	}
-
+	@Override
+	public void run(String... args)  {
+		System.out.printf("roi is %s and cash on cash is %s",
+				kpis.getRoi(),
+				kpis.getCashOnCash());
+	}
 }
